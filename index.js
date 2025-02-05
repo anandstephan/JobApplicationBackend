7e404bf5a0f48e98a25213c747c75fd20107e549
Anand <anandstephan98@gmail.com>
2025-02-05 14:28:15 +0530

    feat:adding controller of application

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const authRoutes = require('./routes/authRoutes');
 const jobRoutes = require('./routes/jobRoutes');
 const companyRoutes = require('./routes/companyRoutes')
+const applicationRoutes = require('./routes/applicationRoutes')
 const app = express()
 
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', jobRoutes);
 app.use('/company',companyRoutes)
+app.use('/application',applicationRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>console.log(`Server is running at ${PORT}`))
