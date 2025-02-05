30107c7e04f65757bed743c37e3c13b21ecbe991
Anand <anandstephan98@gmail.com>
2025-02-05 13:43:55 +0530

    feat:adding main file acces point

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const connectDB = require("./config/db")
-
 const express = require('express')
+
+const authRoutes = require('./routes/authRoutes');
+const jobRoutes = require('./routes/jobRoutes');
 const app = express()
 
 
 connectDB()
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api', jobRoutes);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>console.log(`Server is running at ${PORT}`))
